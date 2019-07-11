5da7fd6bc86aabe1022185e88961d84e4a781d7d
Louis Lammerting <louis.lammerting@gmail.com>
2019-07-11 22:42:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,63 +1,11 @@
-def consolidate_cart(cart)
-  # code here
-  cart_hash = {}
-  cart.each do |cart_item|
-    cart_item.each do |name, data|
-      if cart_hash[name]
-        cart_hash[name][:count] += 1
-      else
-        cart_hash[name] = data
-        cart_hash[name][:count] = 1
-      end
-    end
-  end
-  cart_hash
-end
-
-def apply_coupons(cart, coupons)
-  # code here
-  new_cart = {}
-  cart.each do |grocery, info|
-    coupons.each do |coupon|
-      if grocery == coupon[:item] && info[:count] >= coupon[:num]
-        cart[grocery][:count] = cart[grocery][:count] - coupon[:num]
-        if new_cart[grocery + " W/COUPON"]
-          new_cart[grocery + " W/COUPON"][:count] += 1
-        else
-          new_cart[grocery + " W/COUPON"] = {:price => coupon[:cost], :clearance => cart[grocery][:clearance], :count => 1}
-        end
-      end
-    end
-    new_cart[grocery] = info
-
-  end
-  new_cart
-end
-
-def apply_clearance(cart)
-  # code here
-  clearance_cart = {}
-  cart.each do |item, info|
-    clearance_cart[item] = {}
-    info.each do |datum|
-      if cart[item][:clearance]
-        clearance_cart[item][:price] = (cart[item][:price] * 0.80).round(2)
-      else
-        clearance_cart[item][:price] = cart[item][:price]
-      end
-      clearance_cart[item][:clearance] = cart[item][:clearance]
-      clearance_cart[item][:count] = cart[item][:count]
-
-    end
-  end
-  clearance_cart
-end
-
-
-def checkout(cart: [], coupons: [])
-  cart = consolidate_cart(cart: cart)
-  cart = apply_coupons(cart: cart, coupons: coupons)
-  cart = apply_clearance(cart: cart)
-  final = nil
-
+def consolidate_cart(cart) 
+  new_cart = {} 
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+      new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+  end 
+end 
+new_cart 
 end
